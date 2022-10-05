c9aa845bfa365d33e35c661161bd5f8f4b086aca
Ross Durston <ross.durston@gopuff.com>
2022-10-05 19:11:22 +0100

    Latest changes to try and get it to run quicker

--- _render_batch.R
+++ _render_batch.R
@@ -2,7 +2,13 @@ library(tidyverse)
 library(knitr)
 library(kableExtra)
 
-data_batch <- readr::read_csv("orders_for_rendering.csv")
+
+
+transactionData <- read_csv('ES_Transactions_DataSheet_Compressed.csv') %>%
+  filter(INVOICE_NUMBER==0 | (INVOICE_NUMBER>=1 & INVOICE_NUMBER<=5000) ) %>%
+  write_csv('data_upload_batch_1.csv')
+
+data_batch <- readr::read_csv("data_upload_batch_1.csv")
 
 purrr::walk(
   .x = data_batch$INVOICE_NUMBER,
@@ -12,4 +18,6 @@ purrr::walk(
     params = list(INVOICE_NUMBER = {.x})
   )
 )
+
+
 
